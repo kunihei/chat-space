f46843ec813c17dafdadceb04dfe690c62ed5021
kunihei <kunihei@users.noreply.github.com>
2019-11-26 13:06:24 +0900

    chat-space-group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,14 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
+  def index
+  end
+
   def new
     @group = Group.new
     @group.users << current_user
   end
-  
+
   def create
     @group = Group.new(group_params)
     if @group.save
@@ -13,8 +18,20 @@ class GroupsController < ApplicationController
     end
   end
 
+  def update
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+  end
+
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
